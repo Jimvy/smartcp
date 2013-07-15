8a08dbe920337e837b7187bd5aab72883a6580c5
Benoît Legat <benoit.legat@gmail.com>
2013-07-15 21:45:40 +0200

    Change package name and long_description

--- setup.py
+++ setup.py
@@ -5,25 +5,27 @@ import sys
 from setuptools import setup
 from src.__version__ import version
 
+with open('README.rst') as file:
+  long_description = file.read()
+
 setup(name='smartcp',
   version=version,
   description='Smart Copy Utility',
   author='Benoît Legat',
   author_email='benoit.legat@gmail.com',
   url='http://www.github.com/blegat/smartcp',
-  long_description = """Smart copy utility useful for automating
-  copy to custom source and destination depending on parameters.""",
+  long_description = long_description,
   license = 'GPLv3+',
   install_requires = [
     'PyYAML',
     ],
   #py_modules=['smartcp'],
-  package_dir={'': 'src'},
-  packages=[''],
+  package_dir={'smartcp': 'src'},
+  packages=['smartcp'],
   #scripts=['smartcp.py'],
-  entry_points=dict(console_scripts=['smartcp=smartcp:main', 
+  entry_points=dict(console_scripts=['smartcp=smartcp:main',
     'smartcp-%s=smartcp:main' % sys.version[:3]]),
-  #entry_points=dict(console_scripts=['smartcp=smartcp:main', 
+  #entry_points=dict(console_scripts=['smartcp=smartcp:main',
   #  'smartcp-%s=smartcp:main' % sys.version[:3]]),
   classifiers = [
     'Development Status :: 4 - Beta',
