1db83f9fa90d810023780bb3dff3a4c0416003a9
Benoît Legat <benoit.legat@gmail.com>
2013-07-20 17:45:26 +0200

    Remove useless package architecture

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import sys
 from setuptools import setup
-from src.__version__ import version
+from __version__ import version
 
 with open('README.rst') as file:
   long_description = file.read()
@@ -19,14 +19,9 @@ setup(name='smartcp',
   install_requires = [
     'PyYAML',
     ],
-  #py_modules=['smartcp'],
-  package_dir={'smartcp': 'src'},
-  packages=['smartcp'],
-  #scripts=['smartcp.py'],
-  entry_points=dict(console_scripts=['smartcp=smartcp.smartcp:main',
-    'smartcp-%s=smartcp.smartcp:main' % sys.version[:3]]),
-  #entry_points=dict(console_scripts=['smartcp=smartcp:main',
-  #  'smartcp-%s=smartcp:main' % sys.version[:3]]),
+  py_modules=['smartcp', '__version__'],
+  entry_points=dict(console_scripts=['smartcp=smartcp:main',
+    'smartcp-%s=smartcp:main' % sys.version[:3]]),
   classifiers = [
     'Development Status :: 4 - Beta',
     'Environment :: Console',
--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-# Tells which files to import with
-#   from src import *
-# without it, it would just execute __init__.py
-__all__ = ["__version__", "smartcp"]
