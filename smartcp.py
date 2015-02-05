845617d9b2299609e9afc405d1a339a3d5461c1e
Benoît Legat <benoit.legat@gmail.com>
2015-02-05 14:51:35 +0100

    Add proposition to create missing dir

--- smartcp.py
+++ smartcp.py
@@ -48,6 +48,16 @@ There is NO WARRANTY, to the extent permitted by law.
 
 Written by Benoît Legat.'''.format(program_name, version))
 
+def ask_yes_or_no(prompt):
+  while True:
+    s = input(prompt)
+    if len(s) == 0 or s in ["y", "Y","yes","Yes"]:
+      return True
+    elif s in ["n","N","no","No"]:
+      return False
+    else:
+      print("Please answer 'y' or 'n'.")
+
 def parent_dir_exists(path):
   # we take the absolute path to be to transform './...' in '/...'
   folder = os.path.dirname(os.path.abspath(path))
@@ -57,6 +67,10 @@ def parent_dir_exists(path):
     last = current
     current = os.path.dirname(current)
   if current != folder:
+    if ask_yes_or_no('Create {} in {} ? [Y/n]: '.format(os.path.basename(last), current)):
+      # /!\ race condition, maybe now it exists :(
+      os.makedirs(last)
+      return parent_dir_exists(path)
     print_err('There is no {} in {}'.format(os.path.basename(last), current))
     return False
   return True
